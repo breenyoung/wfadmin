9c678610ca53ae447a1fc126da467887cf53851a
Breen <breen.young@gmail.com>
2016-02-17 00:08:18 -0400

    Iterating

---
+++ resources/assets/js/services/rest.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Breen on 15/02/2016.
+ */
+
+(function(){
+    "use strict";
+
+    angular.module("app.services").factory('RestService', ['$auth', 'Restangular', function($auth, Restangular){
+
+        var baseProducts = Restangular.all('product');
+
+        return {
+
+            getAllProducts: function(scope)
+            {
+                baseProducts.getList().then(function(data)
+                {
+                    //console.log(data);
+                    scope.products = data;
+                });
+            }
+        };
+    }]);
+})();
