bc16bb0088263596cfce7b493c928445138da29d
Breen <breen.young@gmail.com>
2016-02-27 11:28:55 -0400

    Iterating

---
+++ resources/assets/js/app/directives/utc-parser.directive.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('app.directives')
+    .directive('utcParser', function () {
+
+        function link(scope, element, attrs, ngModel) {
+            var parser = function (val) {
+                val = moment.utc(val).format();
+                return val;
+            };
+
+            var formatter = function (val) {
+                if (!val) {
+                    return val;
+                }
+                val = new Date(val);
+                return val;
+            };
+
+            ngModel.$parsers.unshift(parser);
+            ngModel.$formatters.unshift(formatter);
+        }
+
+        return {
+            require: 'ngModel',
+            link: link,
+            restrict: 'A'
+        }
+    });
