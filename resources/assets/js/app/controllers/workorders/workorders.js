4ab6c9724178ad899a642e9d91ebaf821a89c133
Breen <breen.young@gmail.com>
2016-03-01 00:35:16 -0400

    Iterating

--- resources/assets/js/app/controllers/workorders/workorders.js
+++ resources/assets/js/app/controllers/workorders/workorders.js
@@ -8,11 +8,6 @@
         self.showComplete = false;
         var todaysDate = $moment();
 
-        var threeDaysLater = $moment().add(3, 'days');
-        //var sevenDaysLater = $moment().add(7, 'days');
-        //var thirtyDaysLater = $moment().add(30, 'days');
-        var duration3Days = $moment.duration(3, 'days');
-
         RestService.getAllWorkOrders(self);
 
         console.log(self);
