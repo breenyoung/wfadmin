b736817e0b66aa98a17711caddebc04f5145aee9
Breen <breen.young@gmail.com>
2016-03-06 01:27:31 -0400

    Redesign

---
+++ resources/assets/js/app/filters/truncateName.js
@@ -0,0 +1,24 @@
+(function(){
+    "use strict";
+
+    angular.module("app.filters").filter('truncateName', function()
+    {
+        return function(input, maxLength)
+        {
+            input = input || "";
+            var out = "";
+
+            if(input.length > maxLength)
+            {
+                out = input.substr(0, maxLength) + "...";
+            }
+            else
+            {
+                out = input;
+            }
+
+            return out;
+        };
+    });
+
+})();
