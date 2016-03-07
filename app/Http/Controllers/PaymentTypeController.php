d757f0c004c17813afa67fa9aa9c6741f71e32bd
Breen <breen.young@gmail.com>
2016-03-07 09:27:50 -0400

    Laravel stuff for Payment Types

---
+++ app/Http/Controllers/PaymentTypeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\PaymentType;
+
+class PaymentTypeController extends Controller
+{
+    /**
+     * PaymentTypeController constructor.
+     */
+    public function __construct()
+    {
+        // Apply the jwt.auth middleware to all methods in this controller
+        //$this->middleware('jwt.auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $payment_types = PaymentType::orderBy('name', 'asc')->get();
+        return $payment_types;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $payment_type = new PaymentType();
+        $payment_type->name = $request->input('name');
+
+        $payment_type->save();
+
+        return response()->json(['newId' => $payment_type->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $payment_type = PaymentType::find($id);
+
+        return $payment_type;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $payment_type = PaymentType::find($id);
+        if(isset($payment_type))
+        {
+            $payment_type->name = $request->input('name');
+            $payment_type->save();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $payment_type = PaymentType::find($id);
+        if(isset($payment_type))
+        {
+            $payment_type->delete();
+        }
+    }
+}
