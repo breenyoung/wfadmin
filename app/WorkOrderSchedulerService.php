a6f645d2d9f43ffe6c9852834ec4f40195fc9169
Breen <breen.young@gmail.com>
2016-03-07 22:55:29 -0400

    work order scheduler stub

--- app/WorkOrderSchedulerService.php
+++ app/WorkOrderSchedulerService.php
@@ -13,17 +13,23 @@ use Carbon\Carbon;
 
 use App\PurchaseOrder;
 use App\WorkOrder;
+use App\Product;
+use App\Material;
 
-class WorkOrderScheduler
+class WorkOrderSchedulerService
 {
     protected $today;
+    protected $workOrdersDonePerWeek;
+    protected $workOrdersOverflowPerWeek;
 
     /**
-     * WorkOrderScheduler constructor.
+     * WorkOrderSchedulerService constructor.
      */
     public function __construct()
     {
         $this->today = Carbon::today('America/Halifax');
+        $this->workOrdersDonePerWeek = config('scheduling_work_orders_done_per_week');
+        $this->workOrdersOverflowPerWeek = config('scheduling_work_orders_done_per_week_overflow');
     }
 
     public function getWorkOrdersForThisWeek()
@@ -41,6 +47,16 @@ class WorkOrderScheduler
 
         $startOfTheWeek = $date->startOfTheWeek();
         $endOfTheWeek = $date->endOfTheWeek();
+
+        $workOrdersThisWeek = WorkOrder::whereDate('start_date', '>=', $startOfTheWeek)->whereDate('start_date', '<=', $endOfTheWeek)->get();
+
+        $workOrderCount = $workOrdersThisWeek->count();
+
+    }
+
+    public function getProductStock($productIds)
+    {
+        return Product::whereIn('id', $productIds)->select('id', 'current_stock')->get();
     }
 
 
