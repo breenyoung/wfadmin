8226affe9429e7a58286a6e65b9d0bfff23fdfe3
Breen <breen.young@gmail.com>
2016-03-11 21:20:07 -0400

    add filter tools on product summary

--- resources/assets/js/app/controllers/products/products.js
+++ resources/assets/js/app/controllers/products/products.js
@@ -4,8 +4,55 @@
     function ProductController($auth, $state, Restangular, RestService)
     {
         var self = this;
+        self.filterType = "";
+        self.filterOperator = "";
+        self.filterValue = "";
 
         RestService.getAllProducts(self);
+
+        self.applyProductFilter = function(p)
+        {
+            if(self.filterType !== "" && self.filterOperator !== "" && self.filterValue !== "")
+            {
+                console.log("hi");
+                var propertyToFilter = null;
+                switch(self.filterType)
+                {
+                    case "stock":
+                        propertyToFilter = parseInt(p.current_stock);
+                        break;
+                    case "price":
+                        propertyToFilter = parseFloat(p.price);
+                        break;
+                    case "cost":
+                        propertyToFilter = parseFloat(p.cost);
+                        break;
+                }
+
+                if(self.filterOperator === "=")
+                {
+                    return propertyToFilter == parseFloat(self.filterValue);
+                }
+                else if(self.filterOperator === ">")
+                {
+                    return propertyToFilter > parseFloat(self.filterValue);
+                }
+                else if(self.filterOperator === ">=")
+                {
+                    return propertyToFilter >= self.filterValue;
+                }
+                else if(self.filterOperator === "<")
+                {
+                    return propertyToFilter < parseFloat(self.filterValue);
+                }
+                else if(self.filterOperator === "<=")
+                {
+                    return propertyToFilter <= parseFloat(self.filterValue);
+                }
+            }
+
+            return true;
+        };
     }
 
     angular.module('app.controllers').controller('ProductController', ['$auth', '$state', 'Restangular', 'RestService', ProductController]);
