35b8ee8bcadc998a3993c05fe3d846bed48f52c8
Breen <breen.young@gmail.com>
2016-03-13 17:17:49 -0300

    moved email function to another workorderscheduler class

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\WorkOrderSchedulerService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -35,58 +36,8 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function()
         {
-
-            Log::info('=============================================');
-            Log::info('Starting work order reminder service');
-
-            // Setup the dates
-            $pickupDateReminder = config('app.pickup_reminder_days_to_remind');
-            $today = Carbon::today('America/Halifax');
-            $twoDaysFromNow = Carbon::today('America/Halifax')->addDay($pickupDateReminder);
-
-            Log::info('The date is: ' . $today->toFormattedDateString() . ' (America/Halifax)');
-
-            // Get work orders to start today
-            $startWorkOrders = WorkOrder::whereDate('start_date', '=', $today)->get();
-            $startCount = $startWorkOrders->count();
-
-            Log::info('Start Work Orders found: '.$startCount);
-
-            // Get work orders due for pick up in [$pickupDateReminder] days
-            $endWorkOrders = WorkOrder::whereDate('end_date', '<=', $twoDaysFromNow)->with('product', 'customer')->get();
-            $endCount = $endWorkOrders->count();
-            $endDueDate = $twoDaysFromNow->toFormattedDateString();
-
-            Log::info('Sending end reminders for work orders due in [' . $pickupDateReminder . '] day(s) - [' . $endDueDate . ']');
-            Log::info('End Work Orders found: '.$endCount);
-
-            $viewData = [
-                'todaysDate' => $today->toFormattedDateString(),
-                'startCount' => $startCount,
-                'endCount' => $endCount,
-                'startWorkOrders' => $startWorkOrders,
-                'endWorkOrders' => $endWorkOrders,
-                'endDueDate' => $endDueDate,
-                'pickupDateReminder' => $pickupDateReminder
-            ];
-
-            $to = config('app.pickup_reminder_email_to');
-            $from = config('app.pickup_reminder_email_from');
-            $subject = config('app.pickup_reminder_email_subject') . $today->toFormattedDateString();
-            $view = config('app.pickup_reminder_email_view');
-
-            Log::info('Sending work order reminder email to: ' . $to);
-
-            Mail::send($view, ['viewdata' => $viewData], function($message) use($viewData, $to, $from, $subject)
-            {
-                $message->from($from);
-                $message->to($to);
-                $message->subject($subject);
-            });
-
-
-            Log::info('Ending work order reminder service');
-            Log::info('=============================================');
+            $workOrderSchedulerService = new WorkOrderSchedulerService();
+            $workOrderSchedulerService->sendWorkOrderReport();
 
         })->daily()->timezone('America/Halifax');
     }
--- app/WorkOrderSchedulerService.php
+++ app/WorkOrderSchedulerService.php
@@ -10,6 +10,8 @@ namespace App;
 
 use \DB;
 use \Carbon\Carbon;
+use Log;
+use Mail;
 
 use App\PurchaseOrder;
 use App\WorkOrder;
@@ -183,6 +185,62 @@ class WorkOrderSchedulerService
                             ->get();
 
         return $bookedDays;
+    }
+
+    public function sendWorkOrderReport()
+    {
+        Log::info('=============================================');
+        Log::info('Starting work order reminder service');
+
+        // Setup the dates
+        $pickupDateReminder = config('app.pickup_reminder_days_to_remind');
+        $today = Carbon::today('America/Halifax');
+        $twoDaysFromNow = Carbon::today('America/Halifax')->addDay($pickupDateReminder);
+
+        Log::info('The date is: ' . $today->toFormattedDateString() . ' (America/Halifax)');
+
+        // Get work orders to start today
+        $startWorkOrders = WorkOrder::whereDate('start_date', '=', $today)->get();
+        $startCount = $startWorkOrders->count();
+
+        Log::info('Start Work Orders found: '.$startCount);
+
+        // Get work orders due for pick up in [$pickupDateReminder] days
+        $endWorkOrders = WorkOrder::whereDate('end_date', '>=', $today)->whereDate('end_date', '<=', $twoDaysFromNow)->with('product', 'customer')->get();
+        $endCount = $endWorkOrders->count();
+        $endDueDate = $twoDaysFromNow->toFormattedDateString();
+
+        Log::info('Sending end reminders for work orders due in [' . $pickupDateReminder . '] day(s) - [' . $endDueDate . ']');
+        Log::info('End Work Orders found: '.$endCount);
+
+        $viewData = [
+            'todaysDate' => $today->toFormattedDateString(),
+            'startCount' => $startCount,
+            'endCount' => $endCount,
+            'startWorkOrders' => $startWorkOrders,
+            'endWorkOrders' => $endWorkOrders,
+            'endDueDate' => $endDueDate,
+            'pickupDateReminder' => $pickupDateReminder
+        ];
+
+        $to = config('app.pickup_reminder_email_to');
+        $from = config('app.pickup_reminder_email_from');
+        $subject = config('app.pickup_reminder_email_subject') . $today->toFormattedDateString();
+        $view = config('app.pickup_reminder_email_view');
+
+        Log::info('Sending work order reminder email to: ' . $to);
+
+        Mail::send($view, ['viewdata' => $viewData], function($message) use($viewData, $to, $from, $subject)
+        {
+            $message->from($from);
+            $message->to($to);
+            $message->subject($subject);
+        });
+
+
+        Log::info('Ending work order reminder service');
+        Log::info('=============================================');
+
     }
 
     private function getProductStock($productId)
