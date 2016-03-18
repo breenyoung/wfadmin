102dfc35652479345a660bfc8ed66b7eebb84af5
Breen <breen.young@gmail.com>
2016-03-18 14:47:37 -0300

    generated files

--- resources/assets/js/app/app.js
+++ resources/assets/js/app/app.js
@@ -13,7 +13,7 @@
 
     angular.module('app.services', ['ui.router', 'satellizer', 'restangular', 'angular-momentjs', 'ngMaterial']);
     angular.module('app.routes', ['ui.router', 'satellizer']);
-    angular.module('app.controllers', ['ui.router', 'ngMaterial', 'restangular', 'angular-momentjs', 'app.services', 'ngMessages', 'ngMdIcons', 'md.data.table', 'highcharts-ng']);
+    angular.module('app.controllers', ['ui.router', 'ngMaterial', 'restangular', 'angular-momentjs', 'app.services', 'ngMessages', 'ngMdIcons', 'md.data.table', 'highcharts-ng', 'ngCookies']);
     angular.module('app.filters', []);
 
     angular.module('app.directives', ['angular-momentjs']);
